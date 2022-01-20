e3c7ff9f625e37400aa937f804bbc8229936bcbe
Stas <newdom24@gmail.com>
2022-01-20 07:21:17 +0200

    upd style for cf7

--- functions.php
+++ functions.php
@@ -190,8 +190,3 @@ require get_template_directory() . '/inc/template-functions.php';
  * Include Options Panel by Redux.
  */
 require get_template_directory() . '/inc/options-panel-redux.php';
-
-function remove_styles () {
-	wp_deregister_style( 'contact-form-7' );
-}
-add_action ('wp_print_styles', 'remove_styles', 100);
